285f28413bb91a2c5e8749503ec9f84fc0e9a4c1
Gabor Javorszky <gabor@javorszky.co.uk>
2020-12-08 20:20:32 +0000

    add sum and multiplication tables

--- 2020-12-07/main.go
+++ 2020-12-07/main.go
@@ -1,7 +1,17 @@
 package dec72020
 
-import "fmt"
+import (
+	"fmt"
+)
 
+// This week’s question:
+// Given two non-negative integers n1 and n2 represented as strings, return the product of n1 and n2, also represented
+// as a string. Neither input will start with 0, and don’t just convert it to an integer and do the math that way.
+//
+// Examples:
+//
+// $ stringMultiply(“123”, “456”)
+// $ “56088”
 func Task() {
 	fmt.Println("hola")
 }
