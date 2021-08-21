d1ef43414f8ed5c25b54dc05ce54337785d58bad
Gabor Javorszky <gabor@javorszky.co.uk>
2021-08-21 10:21:35 +0100

    Implement palindrome finder

--- 20210816/main.go
+++ 20210816/main.go
@@ -1,6 +1,9 @@
 package august162021
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 func Tasks() {
 	const inc = "badad"
@@ -10,5 +13,36 @@ func Tasks() {
 }
 
 func pSubString(incoming string) string {
-	return ""
+	stringSlice := strings.Split(incoming, "")
+	longest := stringSlice[0]
+
+	var f func(string, int, int) string
+
+	f = func(center string, origin, distance int) string {
+		up := origin + distance
+		down := origin - distance
+
+		// next step would be out of bounds, so whatever we have now is a palindrome that touches at least one edge.
+		if down < 0 || up >= len(stringSlice) {
+			return center
+		}
+
+		// next steps are not the same letter, so it would stop being a palindrome.
+		if stringSlice[down] != stringSlice[up] {
+			return center
+		}
+
+		center = stringSlice[down] + center + stringSlice[up]
+
+		return f(center, origin, distance+1)
+	}
+
+	for i := 1; i < len(stringSlice); i++ {
+		try := f(stringSlice[i], i, 1)
+		if len(try) > len(longest) {
+			longest = try
+		}
+	}
+
+	return longest
 }
