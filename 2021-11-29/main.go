b77d2b438c0cc6a5d18b22d30e0ee2d0df5e2d21
Gabor Javorszky <gabor@javorszky.co.uk>
2021-11-29 10:29:41 +0000

    More work on 29th nov 2021

--- 2021-11-29/main.go
+++ 2021-11-29/main.go
@@ -1,8 +1,10 @@
 package november292021
 
 import (
+	"errors"
 	"fmt"
 	"regexp"
+	"strconv"
 )
 
 func Tasks() {
@@ -26,5 +28,34 @@ func phoneLetter(s string) ([]string, error) {
 		return nil, fmt.Errorf("incoming string is not numbers only, has numbers [0, 1] in them, or is empty: %s", s)
 	}
 
-	return numberMap[2], nil
+	numbers, err := parseNumbers(s)
+	if err != nil {
+		return nil, fmt.Errorf("parsenumbers failed for some weird reason. This should not have happened: %w", err)
+	}
+
+	return assembler("", numbers), nil
+}
+
+func parseNumbers(s string) ([]int, error) {
+	nums := make([]int, len(s))
+	for i, l := range s {
+		n, err := strconv.Atoi(string(l))
+		if err != nil {
+			return nil, fmt.Errorf("parseNumbers: strconv.Atoi for %s: %w", string(l), err)
+		}
+		nums[i] = n
+	}
+	return nums, nil
+}
+
+func assembler(prefix string, numbers []int) []string {
+
+	return nil
+}
+
+func shift(in []int) (int, []int, error) {
+	if len(in) < 1 {
+		return 0, nil, errors.New("incoming slice is too short")
+	}
+	return in[0], in[1:], nil
 }
--- 2021-11-29/main_test.go
+++ 2021-11-29/main_test.go
@@ -50,3 +50,87 @@ func Test_phoneLetter(t *testing.T) {
 		})
 	}
 }
+
+func Test_parseNumbers(t *testing.T) {
+	tests := []struct {
+		name    string
+		s       string
+		want    []int
+		wantErr bool
+	}{
+		{
+			name:    "fails when input string has letters in it",
+			s:       "1234a",
+			want:    nil,
+			wantErr: true,
+		},
+		{
+			name:    "correctly turns string of numbers into slice of ints",
+			s:       "08472772",
+			want:    []int{0, 8, 4, 7, 2, 7, 7, 2},
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := parseNumbers(tt.s)
+			assert.Equal(t, tt.want, got)
+			if tt.wantErr {
+				assert.Error(t, err)
+				return
+			}
+			assert.NoError(t, err)
+		})
+	}
+}
+
+func Test_shift(t *testing.T) {
+	tests := []struct {
+		name      string
+		in        []int
+		want      int
+		want1     []int
+		wantError bool
+	}{
+		{
+			name:      "shifts one element off of the beginning of the array",
+			in:        []int{1, 2, 3, 4, 5},
+			want:      1,
+			want1:     []int{2, 3, 4, 5},
+			wantError: false,
+		},
+		{
+			name:      "shift works fine with a slice of one length",
+			in:        []int{4},
+			want:      4,
+			want1:     []int{},
+			wantError: false,
+		},
+		{
+			name:      "shift works fine with an initialized empty slice",
+			in:        []int{},
+			want:      0,
+			want1:     nil,
+			wantError: true,
+		},
+		{
+			name:      "shift works fine with a nil slice",
+			in:        nil,
+			want:      0,
+			want1:     nil,
+			wantError: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, got1, err := shift(tt.in)
+			assert.Equal(t, tt.want, got)
+			assert.Equal(t, tt.want1, got1)
+			if tt.wantError {
+				assert.Error(t, err)
+				return
+			}
+			assert.NoError(t, err)
+		})
+	}
+}
